16671b95cbaae432ba251e740f2d39e55b95da01
Marta <m.tatsyn@gmail.com>
2026-05-05 15:56:27 +0300

    Add AI habit recommendations and fix analytics metrics

---
+++ HabitFlow/HabitFlow.BLL/Interfaces/IRecommendationService.cs
@@ -0,0 +1,9 @@
+﻿using HabitFlow.BLL.DTOs;
+
+namespace HabitFlow.BLL.Interfaces
+{
+    public interface IRecommendationService
+    {
+        Task<List<RecommendationViewModel>> GetRecommendationsAsync(Guid userId);
+    }
+}
--- HabitFlow/HabitFlow.Web/Controllers/DashboardController.cs
+++ HabitFlow/HabitFlow.Web/Controllers/DashboardController.cs
@@ -6,10 +6,13 @@ namespace HabitFlow.Web.Controllers
     public class DashboardController : Controller
     {
         private readonly IHabitService habitService;
+        private readonly IRecommendationService recommendationService;
 
-        public DashboardController(IHabitService habitService)
+        public DashboardController(IHabitService habitService,
+            IRecommendationService recommendationService)
         {
             this.habitService = habitService;
+            this.recommendationService = recommendationService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +26,8 @@ namespace HabitFlow.Web.Controllers
             var userName = this.HttpContext.Session.GetString("UserName") ?? "Користувач";
             var model = await this.habitService.GetDashboardAsync(
                 Guid.Parse(userId), userName);
+            ViewBag.Recommendations = await this.recommendationService
+                .GetRecommendationsAsync(Guid.Parse(userId));
 
             return this.View(model);
         }
