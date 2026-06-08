ea07a9c88083c1c644f9099f941155fad218f7e2
anastasiiadatsko <anastasiia.datskooo@gmail.com>
2026-06-08 18:49:25 +0300

    Fix empty PWA files

--- HabitFlow/HabitFlow.Web/wwwroot/service-worker.js
+++ HabitFlow/HabitFlow.Web/wwwroot/service-worker.js
@@ -1,1 +1,21 @@
-﻿
+﻿const CACHE_NAME = 'habitflow-v1';
+const urlsToCache = [
+  '/',
+  '/css/site.css',
+  '/js/site.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
